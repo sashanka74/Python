5abefb14299ae543006e236d6f2c38090028f919
Gattamaneni venkata sashank <121079094+sashanka74@users.noreply.github.com>
2025-09-03 10:23:44 +0530

    Dictionary-List(Work)

---
+++ Dictionary(Work).py
@@ -0,0 +1,17 @@
+# .....................................3rd work................................................
+
+l1=[{'First-Name':'Sashank'},{'Last-Name':'Venkata'},{'Age':'22'},{'Gender':'Male'},{'Deggree':'Btech'},{'Branch':'CSE'},{'POY':2030},{'Clg':'SRGEC'},{'Place':'Gudlavalleru'},{'State':'AP'}]
+for item in l1:
+    k= item.keys()
+    for x in k:
+        print(x)  
+
+for item in l1:
+    k= item.values()
+    for x in k:
+        print(x)          
+
+
+
+
+
